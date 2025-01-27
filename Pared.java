1b4f3f596033f9c51900e4e7903d5dc44a75ef7e
AYMEN-ElKHILALY7 <aymelk8@alu.edu.gva.es>
2025-01-27 08:24:06 +0100

    Create Pared.java

---
+++ Pared.java
@@ -0,0 +1,11 @@
+public class Pared {
+    private String direccion;
+
+    public Pared(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+}
